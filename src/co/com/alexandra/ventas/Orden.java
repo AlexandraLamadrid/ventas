801558b85ee6501a395aafc960adc156f2ea0b90
Alexandra <155563331+AlexandraLamadrid@users.noreply.github.com>
2024-01-22 00:07:54 -0500

    Subiendo el programa

---
+++ src/co/com/alexandra/ventas/Orden.java
@@ -0,0 +1,45 @@
+package co.com.alexandra.ventas;
+
+public class Orden {
+    private int idOrden;
+    private Producto productos[];
+    private static int contadorOrdenes;
+    private int contadorProductos;
+    private static final int MAX_PRODUCTOS = 10;
+    
+    //Vamos a agregar el constructor vacio de la clase
+    public Orden(){
+        this.idOrden = ++Orden.contadorOrdenes;
+        this.productos = new Producto[Orden.MAX_PRODUCTOS];
+    }
+    
+    public void agregarProducto(Producto producto){
+        if(this.contadorProductos < Orden.MAX_PRODUCTOS){
+            this.productos[this.contadorProductos++] = producto;
+        }
+        else{
+            System.out.println("Se ha superado el maximo de productos: " + Orden.MAX_PRODUCTOS);
+        }
+    }
+    
+    public double calcularTotal(){
+        double total = 0;
+        for (int i = 0; i < this.contadorProductos; i++) {
+            //Producto producto = this.productos[i];
+            //total += producto.getPrecio();//Esto va a ser igual a total = total + producto.getPrecio()
+            //O en una sola linea, podria ser la siguiente sintaxis:
+            total += this.productos[i].getPrecio();
+        }
+        return total;
+        
+    }
+    public void mostrarOrden(){
+        System.out.println("Id Orden: " + this.idOrden);
+        double totalOrden = this.calcularTotal();
+        System.out.println("Total de la Orden: $" + totalOrden);
+        System.out.println("Productos de la Orden: ");
+        for (int i = 0; i < this.contadorProductos; i++) {
+            System.out.println(this.productos[i]);
+        }
+    }
+}
